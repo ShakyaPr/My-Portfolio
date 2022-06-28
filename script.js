44d7cdc80dd8ab9155a08b01d70a5c5786cc3258
Shakya Rathnaweera <59193561+ShakyaPr@users.noreply.github.com>
2022-06-28 19:11:59 +0530

    Final:v1

--- script.js
+++ script.js
@@ -11,4 +11,22 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 })
+
+// typing text animation script
+var typed = new Typed(".typing",{
+    strings: ["I'm an engineering undergrad", "of University of Moratuwa","in ENTC department"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    loop: true
+})
+var typed = new Typed(".typing2", {
+    strings: ["a tech enthusiast", "an Amateur Developer", "a traveller"],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+});
